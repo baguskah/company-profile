ab7ce39a7e7e71543b184c5d2a06cfc1a9c05cb9
baguskah <dwiutamabagus@gmail.com>
2018-04-26 17:57:18 +0700

    Header

---
+++ resources/views/front-end/front-page.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PT BLST</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    {{-- Bagian Atas --}}
+    <div class="container-fluid" style="background:grey">
+      <div class="row">
+        <div class="container">
+          <div class="col-md-12 header">
+              <div class="row">
+                {{-- Logo --}}
+                <div class="col-md-4">
+                  <div id="logo-perusahaan-warper">
+                    <img id="logo"class="img img-responsive" src="{{asset('images/logo-company-white.png')}}" alt="">
+                  </div>
+                </div>
+
+                {{-- Menu Atas --}}
+                <div class="col-md-4">
+                  <div class="nav navbar" id="navbarNav">
+                    <ul class="">
+                      <li>
+                        <a href="#">Home <span class="sr-only">(current)</span></a>
+                      </li>
+                      <li>
+                        <a href="#">Features</a>
+                      </li>
+                      <li>
+                        <a href="#">Pricing</a>
+                      </li>
+                      <li>
+                        <a href="#">Disabled</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                {{-- Search --}}
+                <div class="col-md-4">
+                  <div class="right-header-panel">
+                    <div id="language-switcher-warper">
+                      <select class="select-lg-transparent" name="">
+                        <option value="">EN</option>
+                        <option value="">ID</option>
+                      </select>
+                    </div>
+                    <div class="search-warper">
+                      <span style="cursor:pointer" class="fa fa-search"></span>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+          </div>
+        </div>
+
+    </div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front-end.front-page');
 });
